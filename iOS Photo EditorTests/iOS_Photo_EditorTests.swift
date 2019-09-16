71c8f707fb66c0009f99a7ea2794aeb5adf24629
Freaky Developer <freakydeveloper04@gmail.com>
2019-09-16 18:42:26 +0530

    Editor

---
+++ iOS Photo EditorTests/iOS_Photo_EditorTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Photo_EditorTests.swift
+//  iOS Photo EditorTests
+//
+//  Created by Vaibhav Mehta on 12/09/19.
+//  Copyright © 2019 oz10. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Photo_Editor
+
+class iOS_Photo_EditorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
